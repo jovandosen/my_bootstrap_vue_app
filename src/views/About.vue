f6e794d4754b0b523fb8e6bcecc9a8404cde2801
jovandosen <jovandosen1994@gmail.com>
2020-06-24 16:59:56 +0200

    Carousel updated

--- src/views/About.vue
+++ src/views/About.vue
@@ -6,28 +6,54 @@
                     id="carousel-fade"
                     style="text-shadow: 0px 0px 2px #000"
                     fade
+                    controls
                     indicators
                     img-width="1024"
                     img-height="480"
                 >
                     <b-carousel-slide
                         caption="First slide"
+                        text="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                        proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
                         img-src="https://picsum.photos/1024/480/?image=10"
                     ></b-carousel-slide>
                     <b-carousel-slide
                         caption="Second Slide"
+                        text="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                        cillum dolore eu fugiat nulla pariatur."
                         img-src="https://picsum.photos/1024/480/?image=12"
                     ></b-carousel-slide>
                     <b-carousel-slide
                         caption="Third Slide"
+                        text="Duis aute irure dolor in reprehenderit in voluptate velit esse
+                        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                        proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
                         img-src="https://picsum.photos/1024/480/?image=22"
                     ></b-carousel-slide>
                     <b-carousel-slide
                         caption="Fourth Slide"
+                        text="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                        proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
                         img-src="https://picsum.photos/1024/480/?image=24"
                     ></b-carousel-slide>
                     <b-carousel-slide
                         caption="Fifth Slide"
+                        text="Ut enim ad minim veniam,
+                        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                        proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
                         img-src="https://picsum.photos/1024/480/?image=25"
                     ></b-carousel-slide>
                 </b-carousel>
