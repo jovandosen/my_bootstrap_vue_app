289f57e89cd3d21307123692c4be408baeb01500
jovandosen <jovandosen1994@gmail.com>
2020-06-24 16:41:43 +0200

    About page updated

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,37 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+    <b-container>
+        <b-row>
+            <b-col>
+                <b-carousel
+                    id="carousel-fade"
+                    style="text-shadow: 0px 0px 2px #000"
+                    fade
+                    indicators
+                    img-width="1024"
+                    img-height="480"
+                >
+                    <b-carousel-slide
+                        caption="First slide"
+                        img-src="https://picsum.photos/1024/480/?image=10"
+                    ></b-carousel-slide>
+                    <b-carousel-slide
+                        caption="Second Slide"
+                        img-src="https://picsum.photos/1024/480/?image=12"
+                    ></b-carousel-slide>
+                    <b-carousel-slide
+                        caption="Third Slide"
+                        img-src="https://picsum.photos/1024/480/?image=22"
+                    ></b-carousel-slide>
+                    <b-carousel-slide
+                        caption="Fourth Slide"
+                        img-src="https://picsum.photos/1024/480/?image=24"
+                    ></b-carousel-slide>
+                    <b-carousel-slide
+                        caption="Fifth Slide"
+                        img-src="https://picsum.photos/1024/480/?image=25"
+                    ></b-carousel-slide>
+                </b-carousel>
+            </b-col>
+        </b-row>
+    </b-container>
 </template>
